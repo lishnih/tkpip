a17f82e097405ab653d7ed0df95883ccf871b2c7
Stan <lishnih@gmail.com>
2013-08-31 10:58:33 +0400

    Исправил несовместимость с Питон 3 в dump.py. Обновил setup.py.

--- setup.py
+++ setup.py
@@ -13,36 +13,39 @@ else:
     if py_version < (2, 6):
         raise RuntimeError('On Python 2, Index requires Python 2.6 or better')
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 try:
     README = open(os.path.join(here, 'README.md')).read()
     CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 except IOError:
     README = CHANGES = ''
 
-install_requires = [
-    ]
 
-from tkpip.lib.info import __version__
+from tkpip.lib.info import __pkgname__, __description__, __version__
+
 
 if __name__ == '__main__':
     setup(
-        name = 'tkpip',
+        name = __pkgname__,
+        description = __description__,
         version = __version__,
+        long_description = __doc__,
+
         author = 'Stan',
         author_email = 'lishnih@gmail.com',
         url = 'http://github.com/lishnih/tkpip',
-        packages = ['tkpip', 'tkpip/lib'],
+        license = 'Public Domain',
+        platforms = 'any',
+        keywords = ['Tk', 'Pip', 'Pypi'],
+
+        packages = [__pkgname__, __pkgname__+'/lib'],
+        package_data = {__pkgname__: []},
         scripts = [
             'scripts/run_tkpip.py',
-            ],
-        package_data = dict(tkpip=[]),
-        description = 'Tk Pip',
-        long_description = __doc__,
-        platforms = 'any',
-        license = 'Public Domain',
-        keywords = ['Tk', 'Pip', 'Pypi'],
-        install_requires = install_requires,
+        ],
+        install_requires = [
+        ],
+
         classifiers = [
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
